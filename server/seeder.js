feb20bdaaba07cdf478d3e8890a61d4258551db1
Minh Khang <minhkhang.tran@gmx.de>
2021-02-28 11:18:35 +0100

    fixing seeder

--- server/seeder.js
+++ server/seeder.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 // models
 import User from "./models/User.js";
 import Profile from "./models/Profile.js";
-//import Post from "./models/Post.js"
+import Post from "./models/Post.js";
 // connectDB
 import connectDB from "./config/db.js";
 
@@ -14,7 +14,7 @@ const destroyData = async () => {
   try {
     await User.deleteMany();
     await Profile.deleteMany();
-    // await Post.deleteMany();
+    await Post.deleteMany();
     console.log("Alles Zerstört".red.inverse);
     process.exit();
   } catch (error) {
